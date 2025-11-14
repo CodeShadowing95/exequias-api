1e0c65230c48b6ef6e7a1c92af6c4491a59508c2
VastoLorde <ksenbonsakura@gmail.com>
2025-11-14 12:12:21 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'server-page.html'));
+})
+
+export default app;

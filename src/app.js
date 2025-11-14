dc55694104e364712d160378666e5c6afee9b73f
VastoLorde <ksenbonsakura@gmail.com>
2025-11-14 17:35:01 +0100

    feat(logging): add winston logger with file transports and middleware
    - Configure winston logger with error and combined log files
    - Add helmet, cors, morgan, cookie-parser middleware
    - Update package.json with new dependencies and imports config
    - Log requests and responses using morgan middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,7 +13,16 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(morgan('combined', { stream: { write: (msg) => logger.info(msg.trim()) } }));
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Exequias API !');
   res.status(200).sendFile(path.join(__dirname, 'server-page.html'));
 });
 
